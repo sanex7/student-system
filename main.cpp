7892f30cb201a9a6cc89fb10731f1f1615cd6777
sanex7 <159659818+sanex7@users.noreply.github.com>
2025-02-03 14:00:03 +0200

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,18 @@
 #include "String.h"
 #include "Contact.h"
+#include "Collage.h"
 #include <iostream>
 
 int main() {
-    String str("Hello, world!");
-    str.Serialize();
-    
-    String newStr;
-    std::cout << "Enter a string: ";
-    newStr.Deserialize();
-    newStr.Serialize();
-    
     Contact contact("123-456-7890", "Kyiv", "Ukraine");
-    contact.Serialize();
+    Collage collage(contact, "Kyiv Polytechnic Institute");
     
-    Contact newContact;
-    std::cout << "Enter contact details (phone city country): ";
-    newContact.Deserialize();
-    newContact.Serialize();
+    collage.Serialize(std::cout);
+    
+    Collage newCollage;
+    std::cout << "Enter collage details (name, phone, city, country):" << std::endl;
+    newCollage.Deserialize(std::cin);
+    newCollage.Serialize(std::cout);
     
     return 0;
 }
