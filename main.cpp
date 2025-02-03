3c35f9cc8c7f2bca2e8fa28019ece869c4904203
sanex7 <159659818+sanex7@users.noreply.github.com>
2025-02-03 14:09:16 +0200

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
 #include "String.h"
 #include "Contact.h"
 #include "Collage.h"
+#include "Student.h"
 #include <iostream>
+#include <fstream>
 
 int main() {
     Contact contact("123-456-7890", "Kyiv", "Ukraine");
     Collage collage(contact, "Kyiv Polytechnic Institute");
+    Student student("Ivan Petrov", "2000-01-01", contact, collage);
+
+    std::ofstream outFile("student.txt");
+    if (outFile) {
+        student.Serialize(outFile);
+        outFile.close();
+    }
+    
+    Student newStudent;
+    std::ifstream inFile("student.txt");
+    if (inFile) {
+        newStudent.Deserialize(inFile);
+        inFile.close();
+    }
+    
+    std::cout << "Deserialized Student:" << std::endl;
+    newStudent.Serialize(std::cout);
     
     collage.Serialize(std::cout);
     
